f72733e039fa10f615783db3b491dc05ad572433
Gillarion <50923922+oiskritsky@users.noreply.github.com>
2021-06-01 13:40:15 +0300

    Update main.js
    Fix let to const

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@
 // test = Boolean(test);
 // console.log(test); // True
 
-// let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+// const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 // let result = 0;
 
 // for (let i = 0; arr.length > i; i++) {
