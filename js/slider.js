7a78d4fff0dd516fe34e31c0b2c27946136585cf
Gillarion <oiskritsky@gmail.com>
2021-07-02 17:44:15 +0300

    fix some mistakes

--- js/slider.js
+++ js/slider.js
@@ -4,8 +4,8 @@ const slidesToScroll = 1;
 const container = document.querySelector(".recommend-list__wrapper");
 const track = document.querySelector(".recommend__list");
 
-let btnPrev = document.querySelector(".btn-prev");
-let btnNext = document.querySelector(".btn-next");
+const btnPrev = document.querySelector(".btn-prev");
+const btnNext = document.querySelector(".btn-next");
 const items = document.querySelectorAll(".recommend__list-item");
 const itemCount = items.length;
 const itemWidth = container.clientWidth / slidesToShow;
@@ -15,24 +15,24 @@ items.forEach((item) => {
   item.style.minWidth = `${itemWidth}px`;
 });
 
-btnPrev.addEventListener("click", () => {
-  const itemsLeft = Math.abs(position) + (slidesToShow * itemWidth) / itemWidth;
-  position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
-  setPosition();
-  checkBtns();
-});
-
-btnNext.addEventListener("click", () => {
-  const itemsLeft = Math.abs(position) / itemWidth;
-  position += itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
-  setPosition();
-  checkBtns();
-});
-
-const setPosition = () => {
-  track.style.transform = `translateX(${position}px)`;
+const btn = function () {
+  btnPrev.addEventListener("click", () => {
+    const itemsLeft = Math.abs(position) / itemWidth;
+    position += itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+    track.style.transform = `translateX(${position}px)`;
+    btnNext.disabled = position <= -(itemCount - slidesToShow) * itemWidth;
+    btnPrev.disabled = position === 0;
+  });
+  btnNext.addEventListener("click", () => {
+    const itemsRight = Math.abs(position) + (slidesToShow * itemWidth) / itemWidth;
+    position -= itemsRight >= slidesToScroll ? movePosition : itemsRight * itemWidth;
+    track.style.transform = `translateX(${position}px)`;
+    btnNext.disabled = position <= -(itemCount - slidesToShow) * itemWidth;
+    btnPrev.disabled = position === 0;
+  });
+  const setPosition = () => {
+    track.style.transform = `translateX(${position}px)`;
+  };
 };
-const checkBtns = () => {
-  btnPrev.disabled = position <= -(itemCount - slidesToShow) * itemWidth;
-  btnNext.disabled = position === 0;
-};
+
+btn();
