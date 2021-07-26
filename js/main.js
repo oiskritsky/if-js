aa9c855055212ced5c7791ec681b778ed2e65890
Gillarion <oiskritsky@gmail.com>
2021-07-26 14:44:55 +0300

    done 11 lesson task

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+/* eslint-disable comma-dangle */
 /* eslint-disable no-unused-expressions */
 /* eslint-disable no-unused-vars */
 /// /////////////////////
@@ -5,10 +7,10 @@ const pickerForm = document.querySelector('.form-desktop-people__picker');
 const peoplePicker = document.querySelector('.people-picker');
 const childrenPicker = document.querySelector('.picker__children');
 const childrenAgePicker = document.querySelector('.picker__age');
+const childrenAgePickerSelect = document.querySelector('#age');
 const adultsInput = document.querySelector('#adults');
 const childrenInput = document.querySelector('#children');
 const roomsInput = document.querySelector('#rooms');
-
 /// ////////////////////
 const plusAdults = document.querySelector('.picker__adults button.plus');
 const plusChildren = document.querySelector('.picker__children button.plus');
@@ -19,24 +21,103 @@ const minusChildren = document.querySelector('.picker__children button.minus');
 const minusRooms = document.querySelector('.picker__rooms button.minus');
 /// ///////////////////
 let adultsCount = 0;
-const childrenCount = 0;
-const roomsCount = 0;
+let childrenCount = 0;
+let roomsCount = 0;
 /// ////////////////////
+
+// Установка значений в placeholder
 const setValue = () => {
   peoplePicker.setAttribute(
     'placeholder',
-    `Adults - ${adultsCount} Child - ${childrenCount} Rooms - ${roomsCount}`,
+    `Adults - ${adultsCount} Child - ${childrenCount} Rooms - ${roomsCount}`
   );
 };
 setValue();
 /// /////////////////////
-const visibleClass = (e) => {
+
+// присвоение класса нашему блоку picker при выполнении
+const setPickerVisibleClass = () => {
   pickerForm.classList.toggle('visible');
 };
-peoplePicker.addEventListener('click', visibleClass);
-/// ///////////////////////
+
+// присвоение класса нашему блоку picker при клике
+peoplePicker.addEventListener('click', setPickerVisibleClass);
+/// ////////////////////////////////////////////////
+
+const clickChildren = (e) => {
+  e.preventDefault(); // отключаем дефолтное поведение браузера при клике по button
+  /// /////////////////////////////
+  if (e.target.classList.contains('plus')) {
+    childrenCount += 1;
+    childrenInput.value = childrenCount;
+  } else if (e.target.classList.contains('minus')) {
+    childrenCount -= 1;
+    childrenInput.value = childrenCount;
+  }
+  /// /////////////////////////////
+  if (childrenInput.value <= 0) {
+    childrenCount = 0;
+    childrenInput.value = 0;
+    minusChildren.style.border = '1px solid #CECECE';
+    minusChildren.style.color = '#CECECE';
+    minusChildren.disabled;
+    minusChildren.removeEventListener('click', clickChildren, true);
+  } else {
+    minusChildren.style.border = '1px solid #3077c6';
+    minusChildren.style.color = '#3077c6';
+  }
+  /// /////////////////////////////
+  if (childrenInput.value >= 10) {
+    childrenCount = 10;
+    childrenInput.value = 10;
+    plusChildren.style.border = '1px solid #CECECE';
+    plusChildren.style.color = '#CECECE';
+    plusChildren.disabled;
+    plusChildren.removeEventListener('click', clickChildren, true);
+  } else {
+    plusChildren.style.border = '1px solid #3077c6';
+    plusChildren.style.color = '#3077c6';
+  }
+  /// /////////////////////////////
+  setValue();
+};
+
+plusChildren.addEventListener('click', clickChildren);
+minusChildren.addEventListener('click', clickChildren);
+
+/// /////////////////////////////////////////////////////////
+const childrenAgePickerShow = (e) => {
+  if (childrenInput.value >= 1) {
+    childrenAgePicker.style.display = 'block';
+  }
+  for (let i = childrenInput.value; i >= 2 && i <= 10; i += 1) {
+    if (i < 10) {
+      const selectClone = childrenAgePickerSelect.cloneNode(true);
+      selectClone.id = '';
+      selectClone.classList = 'inner';
+      childrenAgePickerSelect.parentNode.insertBefore(
+        selectClone,
+        childrenAgePickerSelect.nextSibling
+      );
+    }
+  }
+};
+
+const childrenAgePickerHide = (e) => {
+  const removeLastSelect = document.querySelector('select.inner:last-child');
+  if (childrenInput.value < 1) {
+    childrenAgePicker.style.display = 'none';
+  } else if (childrenInput.value >= 1) {
+    removeLastSelect.remove();
+  }
+};
+
+plusChildren.addEventListener('click', childrenAgePickerShow);
+minusChildren.addEventListener('click', childrenAgePickerHide);
+
+/// /////////////////////////////////////////////////////////
 const clickAdults = (e) => {
-  e.preventDefault();
+  e.preventDefault(); // отключаем дефолтное поведение браузера при клике по button
 
   if (e.target.classList.contains('plus')) {
     adultsCount += 1;
@@ -72,3 +153,41 @@ const clickAdults = (e) => {
 
 plusAdults.addEventListener('click', clickAdults);
 minusAdults.addEventListener('click', clickAdults);
+
+const clickRooms = (e) => {
+  e.preventDefault(); // отключаем дефолтное поведение браузера при клике по button
+
+  if (e.target.classList.contains('plus')) {
+    roomsCount += 1;
+    roomsInput.value = roomsCount;
+  } else if (e.target.classList.contains('minus')) {
+    roomsCount -= 1;
+    roomsInput.value = roomsCount;
+  }
+
+  if (roomsInput.value <= 0) {
+    roomsCount = 0;
+    roomsInput.value = 0;
+    minusRooms.style.border = '1px solid #CECECE';
+    minusRooms.style.color = '#CECECE';
+    minusRooms.disabled;
+  } else {
+    minusRooms.style.border = '1px solid #3077c6';
+    minusRooms.style.color = '#3077c6';
+  }
+  if (roomsInput.value >= 30) {
+    roomsCount = 30;
+    roomsInput.value = 30;
+    plusRooms.style.border = '1px solid #CECECE';
+    plusRooms.style.color = '#CECECE';
+    plusRooms.disabled;
+  } else {
+    plusRooms.style.border = '1px solid #3077c6';
+    plusRooms.style.color = '#3077c6';
+  }
+
+  setValue();
+};
+
+plusRooms.addEventListener('click', clickRooms);
+minusRooms.addEventListener('click', clickRooms);
